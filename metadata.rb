e00075644b86e3219f8ab5c338bf68e9a132d18c
Aaron D. Valade <adv@alum.mit.edu>
2012-05-03 13:51:44 +0800

    bumped version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "chris.peplin@rhubarbtech.com"
 license          "Apache 2.0"
 description      "Installs/Configures asterisk"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.mkd'))
-version          "0.5.0-gs"
+version          "0.6.0"
 
 supports "ubuntu"
 
